d76c74dee5e68c7ad07fde24d27fffd4fe9e7ddf
Ray.Lin <Ray.Lin@chinasoftinc.com>
2018-03-19 15:45:05 +0800

    0319 增加自定义premain代理类

---
+++ demo-agent/src/main/java/com/ray/agent/MyAgent.java
@@ -0,0 +1,20 @@
+package com.ray.agent;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * 自定义代理类
+ * @author Ray
+ * @createTime 2018-03-19 14:07
+ */
+public class MyAgent {
+    public static void  permain(String agentOps, Instrumentation inst){
+        System.out.println("=========premain方法执行========");
+        System.out.println(agentOps);
+    }
+
+    public static void premain(String agentOps) {
+        System.out.println("=========premain方法执行2========");
+        System.out.println(agentOps);
+    }
+}
